31bed708528dcefca8df2daa931b5b775100a7b4
nampt <nampt@Phams-MacBook-Pro.local>
2022-08-15 01:48:40 +0700

    feat: 🎸 setup palette (colors, font, icons)

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,31 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: ["Roboto", "sans-serif"],
+    },
+    extend: {
+      colors: {
+        info: {
+          DEFAULT: "#FFFFFF",
+        },
+        normal: {
+          DEFAULT: "#86D03D",
+        },
+        advisory: {
+          DEFAULT: "#FFDB7D",
+        },
+        warning: {
+          DEFAULT: "#EF7300",
+        },
+        critical: {
+          DEFAULT: "#FF0BBB",
+        },
+        emergency: {
+          DEFAULT: "#D10000",
+        },
+      },
+    },
   },
   plugins: [],
 };
